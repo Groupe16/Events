eda28ab8c440349399a70503e831ff654ec9490f
Zalila <ay.zalila@gmail.com>
2015-04-21 13:48:19 -0400

    13:48

--- app/src/main/java/com/localisation/events/model/Account.java
+++ app/src/main/java/com/localisation/events/model/Account.java
@@ -7,9 +7,67 @@ import java.sql.Time;
  */
 public class Account {
     private String login;
+    private String email;
+    private String phone;
     private String password;
     private User user;
     private String device;
     private Time lastConnection;
 
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public Time getLastConnection() {
+        return lastConnection;
+    }
+
+    public void setLastConnection(Time lastConnection) {
+        this.lastConnection = lastConnection;
+    }
 }
